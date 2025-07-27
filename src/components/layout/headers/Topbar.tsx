379749595212af7a785fd0caacb4bf7d16607e9a
Chukwuyem Obiazi <careplexus01@gmail.com>
2025-07-27 04:41:24 +0100

    chore: topbar logo

--- src/components/layout/headers/Topbar.tsx
+++ src/components/layout/headers/Topbar.tsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 export default function Topbar() {
     return (
         <section className="bg-white shadow-md px-4 py-2 flex justify-between items-center">
-            <div className="flex items-center space-x-2">
-                <PhoneCallIcon className="text-green-500" size={20} aria-hidden="true" />
-                <span className="text-sm text-gray-700 font-medium">0816 829 6445</span>
-            </div>
-             <div className="md:hidden flex items-center gap-1">
+            <div className="md:hidden flex items-center gap-1">
                 <Link to="/" className="flex items-center gap-1">
                     <h1 className="text-2xl font-bold text-gray-800">CPLX</h1>
                     <PlusCircle className="text-blue-500" size={20} />
                 </Link>
+            </div>
+            <div className="flex items-center space-x-2">
+                <PhoneCallIcon className="text-green-500" size={20} aria-hidden="true" />
+                <span className="text-sm text-gray-700 font-medium">0816 829 6445</span>
             </div>
             <div className="flex items-center space-x-3">
                 <Link
