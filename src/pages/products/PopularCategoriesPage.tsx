c88b1408f54028c77f3d0b475340e77cef26072c
Chukwuyem Obiazi <careplexus01@gmail.com>
2025-08-26 12:04:32 +0100

    Update PopularCategoriesPage.tsx

--- src/pages/products/PopularCategoriesPage.tsx
+++ src/pages/products/PopularCategoriesPage.tsx
@@ -10,7 +10,7 @@ export default function PopularCategoriesPage() {
     <div className="grid grid-cols-1 md:grid-cols-4 gap-4 md:px-2 pb-10 max-w-7xl mx-auto">
       
       <main className="md:col-span-3 space-y-6">
-      <h1 className=" text-xl text-center md:text-left md:text-3xl font-bold tracking-wide mb-2 text-black bg-blue-200 rounded-sm py-4">
+      <h1 className=" text-xl text-center md:text-left md:text-3xl font-bold tracking-wide pl-2 mb-2 text-black bg-blue-200 rounded-sm py-4">
   SMART PHARMACY CATALOGUE
 </h1>
 
